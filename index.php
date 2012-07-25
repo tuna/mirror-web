96caf27935ee994deb370e085aee9bf4a7d3e4a5
Qijiang Fan <fqj1994@gmail.com>
2012-07-25 18:03:56 +0800

    添加了是否为官方镜像的信息

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include "includes/bydistro.php";
 <head>
 	<meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
 	<link href="files/mirrors.tuna.css" rel="stylesheet" type="text/css" />	
+	<link href="files/fontawesome/css/font-awesome.css" rel="stylesheet" type="text/css" />	
 	<script type="text/javascript" src="files/jquery-latest.js"></script>
 	<script type="text/javascript" src="files/__jquery.tablesorter.min.js"></script>
 	<script type="text/javascript">
@@ -63,15 +64,18 @@ include "includes/bydistro.php";
 			type: 'numeric'
 		});
 		$('#status-main-table').tablesorter({
-			sortList: [[0,0]],
+			sortList: [[1,0]],
 			headers : {
-				2: {
-					'sorter' : 'status'
+				0: {
+					'sorter' : false
 				},
 				3: {
+					'sorter' : 'status'
+				},
+				4: {
 					'sorter' : 'size'
 				},
-				6: {
+				7: {
 					'sorter' : 'httprequest'
 				}
 			}
@@ -116,41 +120,43 @@ $status = initialize_status(array(
 	'/home/mirror/log/status.txt',
 	'http://ftp3.tsinghua.edu.cn/newstatus.txt'));
 $specs = array(
-	array('apache', 'apache software foundation的软件', 'fqj1994'),
-	array('archlinux', '滚动更新的 Linux 发行版，极简主义哲学。', 'xiaq'),
-	array('archlinuxarm', 'Archlinux ARM port.', 'xiaq'),
-	array('centos', '由社区维护的与 RHEL 完全兼容的发行版。', 'alick'),
-	array('chakra', '基于 KDE SC、无 Gtk 的桌面环境。前身是 Archlinux 的 [kde-mod]。', 'xiaq'),
-	array('CPAN', 'Comprehensive Perl Archive Network。', 'fqj1994'),
-	array('CTAN', 'Comprehensive TeX Archive Network。', 'MichaelChou'),
-	array('cygwin', 'Windows 平台下的类 Unix环境.', 'BYVoid'),
-	array('debian', '一个完全由社区维护的 Linux 发行版。', 'heroxbd'),
-	array('debian-backports', 'Debian Stable 上用的 Testing/Unstable 扩展包。', 'heroxbd'),
-	array('debian-cd', 'Debian CD 镜像。', 'fqj1994'),
-	array('debian-multimedia', 'Debian 非官方多媒体套件。', 'heroxbd'),
-	array('debian-security', 'Debian 安全情报', 'heroxbd'),
-	array('debian-weekly-builds', 'Debian CD 镜像每周构建。', 'fqj1994'),
-	array('deepin', '基于 Ubuntu 的面向中文桌面用户的发行版。', 'xiaq'),
-	array('deepin-releases', 'Deepin 稳定版的 CD 镜像。', 'xiaq'),
-	array('epel', 'Fedora 社区为 RHEL 等提供的额外软件包。', 'BYVoid'),
-	array('fedora', '自由友爱杰出前卫的 Linux 发行版，Red Hat 公司赞助的社区项目。', 'BYVoid'),
-	array('freebsd', '拥有辉煌历史的 BSD 的一个重要分支。', 'xiaq'),
+	//第四个（下标3）表示是否为官方源，N表示不是，A表示是（并且官方分配了另外的域名）
+	//B表示是（但是没有官方的域名），U表示不知道
+	array('apache', 'apache software foundation的软件', 'fqj1994', array('N')),
+	array('archlinux', '滚动更新的 Linux 发行版，极简主义哲学。', 'xiaq', array('N')),
+	array('archlinuxarm', 'Archlinux ARM port.', 'xiaq', array('A', 'url' => 'cn.mirror.archlinuxarm.org')),
+	array('centos', '由社区维护的与 RHEL 完全兼容的发行版。', 'alick', array('B')),
+	array('chakra', '基于 KDE SC、无 Gtk 的桌面环境。前身是 Archlinux 的 [kde-mod]。', 'xiaq', array('N')),
+	array('CPAN', 'Comprehensive Perl Archive Network。', 'fqj1994', array('N')),
+	array('CTAN', 'Comprehensive TeX Archive Network。', 'MichaelChou', array('N')),
+	array('cygwin', 'Windows 平台下的类 Unix环境.', 'BYVoid', array('N')),
+	array('debian', '一个完全由社区维护的 Linux 发行版。', 'heroxbd', array('N')),
+	array('debian-backports', 'Debian Stable 上用的 Testing/Unstable 扩展包。', 'heroxbd', array('N')),
+	array('debian-cd', 'Debian CD 镜像。', 'fqj1994', array('N')),
+	array('debian-multimedia', 'Debian 非官方多媒体套件。', 'heroxbd', array('N')),
+	array('debian-security', 'Debian 安全情报', 'heroxbd', array('N')),
+	array('debian-weekly-builds', 'Debian CD 镜像每周构建。', 'fqj1994', array('N')),
+	array('deepin', '基于 Ubuntu 的面向中文桌面用户的发行版。', 'xiaq', array('B')),
+	array('deepin-releases', 'Deepin 稳定版的 CD 镜像。', 'xiaq', array('B')),
+	array('epel', 'Fedora 社区为 RHEL 等提供的额外软件包。', 'BYVoid', array('N')),
+	array('fedora', '自由友爱杰出前卫的 Linux 发行版，Red Hat 公司赞助的社区项目。', 'BYVoid', array('N')),
+	array('freebsd', '拥有辉煌历史的 BSD 的一个重要分支。', 'xiaq', array('N')),
 	// array('frugalware', 'Slackware 和 Archlinux 哲学的混血，独特的半滚动发行模式。', 'xiaq'),
-	array('gentoo', '一个快速的元发行版，软件包系统使用源代码。', 'cuckoo'),
-	array('gentoo-portage', 'Gentoo 的 ports collection。', 'cuckoo'),
-	array('gentoo-portage-prefix', 'Gentoo on a different level', 'heroxbd'),
-	array('gnu', 'GNU/Linux 的基础软件。', 'MichaelChou'),
-	array('kernel', 'Linux 内核。', 'BYVoid'),
-	array('linuxmint', '基于Ubuntu的发行版', 'fqj1994'),
-	array('linuxmint-cd', 'LinuxMint的CD/DVD镜像', 'fqj1994'),
-	array('macports', 'Mac OS X 与 Darwin 的包管理软件，GUI与CLI的结合。', 'VuryLeo'),
-	array('opensuse', '由 Novell 支持的 Linux 发行版。', 'xiaq'),
-	array('pypi', 'Python Package Index', 'fqj1994'),
-	array('rpmfusion', '一个用于 Fedora 和 RHEL 等的第三方软件仓库。', 'alick'),
-	array('scientific', '由美国费米实验室维护的与 RHEL 兼容的发行版。', 'BYVoid'),
-	array('slackware', '最有资历的 Linux 发行版。', 'BYVoid'),
-	array('ubuntu', '基于 Debian 的以桌面应用为主的 Linux 发行版。', 'BYVoid'),
-	array('ubuntu-releases', 'Ubuntu CD 镜像。', 'MichaelChou'),
+	array('gentoo', '一个快速的元发行版，软件包系统使用源代码。', 'cuckoo', array('N')),
+	array('gentoo-portage', 'Gentoo 的 ports collection。', 'cuckoo', array('N')),
+	array('gentoo-portage-prefix', 'Gentoo on a different level', 'heroxbd', array('A', 'url' => 'rsync.cn.prefix.freens.org')),
+	array('gnu', 'GNU/Linux 的基础软件。', 'MichaelChou', array('N')),
+	array('kernel', 'Linux 内核。', 'BYVoid', array('N')),
+	array('linuxmint', '基于Ubuntu的发行版', 'fqj1994', array('N')),
+	array('linuxmint-cd', 'LinuxMint的CD/DVD镜像', 'fqj1994', array('N')),
+	array('macports', 'Mac OS X 与 Darwin 的包管理软件，GUI与CLI的结合。', 'VuryLeo', array('N')),
+	array('opensuse', '由 Novell 支持的 Linux 发行版。', 'xiaq', array('N')),
+	array('pypi', 'Python Package Index', 'fqj1994', array('N')),
+	array('rpmfusion', '一个用于 Fedora 和 RHEL 等的第三方软件仓库。', 'alick', array('N')),
+	array('scientific', '由美国费米实验室维护的与 RHEL 兼容的发行版。', 'BYVoid', array('N')),
+	array('slackware', '最有资历的 Linux 发行版。', 'BYVoid', array('N')),
+	array('ubuntu', '基于 Debian 的以桌面应用为主的 Linux 发行版。', 'BYVoid', array('N')),
+	array('ubuntu-releases', 'Ubuntu CD 镜像。', 'MichaelChou', array('N')),
 );
 
 function maintainer($name)
@@ -216,6 +222,7 @@ function format_size($size)
 <table class="tablesorter" id="status-main-table">
 	<thead>
 	<tr>
+		<th>官方</th>
 		<th>名称</th>
 		<th>维护者</th>
 		<th>状态</th>
@@ -229,6 +236,23 @@ function format_size($size)
 <?php foreach ($specs as $spec): ?>
 	<?php $info = $status[$spec[0]] ?>
 	<tr>
+	<td><?php
+switch ($spec[3][0]) {
+case 'A':
+	echo '<a href="http://', $spec[3]['url'] ,'" target="_blank"><img height="16px" width="16px" src="files/yes.png"/></a>';
+	break;
+case 'B':
+	echo '<img height="16px" width="16px" src="files/yes.png"/>';
+	break;
+case 'N':
+	echo '<img height="16px" width="16px" src="files/no.png"/>';
+	break;
+case 'U':
+	echo '<img height="16px" width="16px" src="files/unknown.png"/>';
+	break;
+}
+?></td>
+
 		<td>
 		<a href="<?php echo $spec[0] ?>/" title="<?php echo $spec[1] ?>">
 				<?php echo $spec[0] ?>
