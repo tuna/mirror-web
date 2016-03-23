344dcb2f127e10bd3f3df07f6eb1bcb5787c4372
bigeagle <justin.w.xd@gmail.com>
2016-03-23 21:57:50 +0800

    auto generate robots.txt

---
+++ gen_robot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo '# robots.txt for https://mirrors.tuna.tsinghua.edu.cn' > robots.txt
+echo 'User-agent: *' >> robots.txt
+echo '' >> robots.txt
+
+curl -s https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json | jq -r '.[] | .name' | while read name; do
+	echo "Disallow: /${name}" >> robots.txt
+done
