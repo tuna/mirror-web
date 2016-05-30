2368ee850c6a288bda07313d9d9c7557bf26928c
bigeagle <justin.w.xd@gmail.com>
2016-05-30 15:15:49 +0800

    update robots.txt

--- gen_robot.sh
+++ gen_robot.sh
@@ -4,6 +4,6 @@ echo '# robots.txt for https://mirrors.tuna.tsinghua.edu.cn' > robots.txt
 echo 'User-agent: *' >> robots.txt
 echo '' >> robots.txt
 
-curl -s https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json | jq -r '.[] | .name' | while read name; do
+curl -s https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json | jq -r '.[] | .name' | uniq | while read name; do
 	echo "Disallow: /${name}" >> robots.txt
 done
