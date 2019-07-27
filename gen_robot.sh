cdc62d5657e42eb9daf72d5c95f71139f79c7267
王邈 <shankerwangmiao@gmail.com>
2019-07-27 00:46:17 -0400

    Announce open log
    Signed-off-by: 王邈 <shankerwangmiao@gmail.com>

--- gen_robot.sh
+++ gen_robot.sh
@@ -3,6 +3,7 @@
 echo '# robots.txt for https://mirrors.tuna.tsinghua.edu.cn' > robots.txt
 echo 'User-agent: *' >> robots.txt
 echo '' >> robots.txt
+echo 'Disallow: /logs' >> robots.txt
 
 curl -s https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json | jq -r '.[] | .name' | uniq | while read name; do
 	[[ -z ${name} ]] && continue
