551035bd8944d99d476e75e7b0511e2beb433bd1
Jiajie Chen <noc@jiegec.ac.cn>
2020-03-22 22:47:00 +0800

    Use variable HOST in gen_robot.sh

--- gen_robot.sh
+++ gen_robot.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+HOST="mirrors.tuna.tsinghua.edu.cn"
 
-echo '# robots.txt for https://mirrors.tuna.tsinghua.edu.cn' > robots.txt
+echo "# robots.txt for https://$HOST" > robots.txt
 echo 'User-agent: *' >> robots.txt
 echo '' >> robots.txt
 echo 'Disallow: /logs' >> robots.txt
 
-curl -s https://mirrors.tuna.tsinghua.edu.cn/static/tunasync.json | jq -r '.[] | .name' | uniq | while read name; do
+curl -s https://$HOST/static/tunasync.json | jq -r '.[] | .name' | uniq | while read name; do
 	[[ -z ${name} ]] && continue
 	echo "Disallow: /${name}" >> robots.txt
 done
