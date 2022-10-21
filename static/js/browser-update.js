e8f279712ce064ec992673d9d1f122cd22629146
Zenithal <i@zenithal.me>
2022-10-22 00:51:34 +0800

    remove browser-update

--- static/js/browser-update.js
+++
@@ -1,8 +0,0 @@
-var $buoop = {vs:{i:10,f:68,o:44,s:8,c:57},unsecure:true,api:4};
-function $buo_f(){ 
- var e = document.createElement("script"); 
- e.src = "//browser-update.org/update.min.js"; 
- document.body.appendChild(e);
-};
-try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-catch(e){window.attachEvent("onload", $buo_f)}
