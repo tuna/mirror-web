4639ea911e8029f2f23c48ddff66763a678b2eb1
Miao Wang <shankerwangmiao@gmail.com>
2023-08-13 00:31:31 +0800

    genrobot: also disallow alias
    Signed-off-by: Miao Wang <shankerwangmiao@gmail.com>

--- gen_robot.sh
+++ gen_robot.sh
@@ -6,7 +6,15 @@ echo 'User-agent: *' >> robots.txt
 echo '' >> robots.txt
 echo 'Disallow: /logs' >> robots.txt
 
-curl -s https://$HOST/static/tunasync.json | jq -r '.[] | .name' | uniq | while read name; do
-	[[ -z ${name} ]] && continue
+(
+  curl -s https://$HOST/static/tunasync.json | jq -r '.[] | .name'
+  echo "lede";
+  echo "raspberry-pi-os";
+  echo "ctan";
+  echo "cygwin";
+  echo "pub";
+  echo "git";
+) | uniq | while read name; do
+	[[ -z ${name} ]] || [[ ${name} = "tuna" ]] && continue
 	echo "Disallow: /${name}" >> robots.txt
 done
