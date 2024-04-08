12d8cf1fa773ec0165c56d2156754c4a0ceb078d
Miao Wang <shankerwangmiao@gmail.com>
2024-04-08 15:15:04 +0800

    vite.config: also export help pages

--- _vite.config.mjs
+++ _vite.config.mjs
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 import ruby from 'vite-plugin-ruby'
 import components from 'unplugin-vue-components/vite'
 import legacy from '@vitejs/plugin-legacy'
+const exposedData = ['config', 'data', 'categories'];
+const jekyllData = Object.fromEntries(exposedData.map((key) => [key, JSON.parse(process.env[`site_${key}`] || '{}')]));
 
 export default defineConfig(({mode})=>({
   build: {
@@ -13,8 +15,6 @@ export default defineConfig(({mode})=>({
   },
   plugins: [
     (()=>{
-      const exposedData = ['config', 'data', 'categories'];
-      const jekyllData = Object.fromEntries(exposedData.map((key) => [key, JSON.parse(process.env[`site_${key}`] || '{}')]));
       const importNamePrefix = 'virtual:jekyll-';
       const loadNamePrefix = '\0' + importNamePrefix;
 
@@ -28,7 +28,27 @@ export default defineConfig(({mode})=>({
         load(id) {
           if (id.startsWith(loadNamePrefix)) {
             const key = id.slice(loadNamePrefix.length);
-            return Object.entries(jekyllData[key]).map(([key, value]) => `export const ${key} = ${JSON.stringify(value)};`).join('\n');
+            return Object.entries(jekyllData[key]).map(([key, value]) => `export const ${key.replace("-", "_")} = ${JSON.stringify(value)};`).join('\n');
+          }
+        },
+      };
+    })(),
+    (()=>{
+      const helpPages = jekyllData.categories.help || [];
+      return {
+        name: 'tuna-help-pages',
+        resolveId(id) {
+          if (id === "virtual:tuna-help-pages") {
+            return '\0' + id;
+          }
+        },
+        load(id) {
+          if (id === '\0' + "virtual:tuna-help-pages") {
+            const pages = Object.fromEntries(helpPages.map((page) => [
+              page.mirrorid,
+              page.url,
+            ]));
+            return `export default ${JSON.stringify(pages)};`;
           }
         },
       };
