0da5a1739ab88cc8e21b1adf4524996902f21ebf
Miao Wang <shankerwangmiao@gmail.com>
2024-04-09 23:15:58 +0800

    move scripts to _src/

--- _src/entrypoints/default.js
+++ _src/entrypoints/default.js
@@ -12,3 +12,11 @@ if (siteSuffix) {
     });
   }
 }
+
+const lei3Po8h = ["support", ["tuna", "tsinghua", "edu", "cn"].join(".")].join(
+  "@",
+);
+Array.from(document.querySelectorAll("a.eib1gieB")).forEach((el) => {
+  el.textContent = lei3Po8h;
+  el.href = ["ma", "ilto:"].join("i") + lei3Po8h;
+});
