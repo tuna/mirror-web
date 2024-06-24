55d34b3d71da870e6f8ad21a10de29992f5b3099
Miao Wang <shankerwangmiao@gmail.com>
2024-06-24 11:54:26 +0800

    app: fix BootstrapModal import error
    Fixes: c89c3506
    Fixes: #434
    
    Signed-off-by: Miao Wang <shankerwangmiao@gmail.com>

--- _src/entrypoints/app.ts
+++ _src/entrypoints/app.ts
@@ -2,7 +2,7 @@ import Empty from "../components/Empty.vue";
 import IsoModal from "../components/IsoModal.vue";
 import MainMirrorList from "../components/MainMirrorList.vue";
 
-import { BootStrapModal } from "bootstrap/js/dist/modal";
+import BootStrapModal from "bootstrap/js/dist/modal";
 import { createApp } from "vue";
 import "./default";
 import "../styles/main-page.scss";
