091a7d022bd52c88e7cb776ac3f71b7a428b8d35
Miao Wang <shankerwangmiao@gmail.com>
2026-02-21 17:12:37 +0800

    [WIP] helpz: remove unused code

--- _src/entrypoints/help.ts
+++ _src/entrypoints/help.ts
@@ -5,57 +5,11 @@ import {
 } from "virtual:jekyll-config";
 import { options as globalOptions } from "virtual:jekyll-data";
 import hljs from "../lib/hljs";
-import Mark from "markup-js";
 import { TUNASYNC_JSON_PATH } from "../lib/consts";
-import { mirrorId } from "../lib/mirrorid";
 import "./default";
 import "../styles/help.scss";
 
-Array.from(document.querySelectorAll("#help-content table")).map((el) => {
-  el.classList.add("table", "table-bordered", "table-striped");
-});
-
-const update_target = (ev) => {
-  const sel = ev.target;
-  const target_selectors = sel.attributes["data-target"].value.split(",");
-  for (const target_selector of target_selectors) {
-    const target = document.querySelector(target_selector);
-    const template_selector = target.attributes["data-template"].value;
-    const select_selectors = target.attributes["data-select"].value.split(",");
-    let url = "/" + mirrorId;
-    if (mirrorId.endsWith(".git")) {
-      url = "/git/" + mirrorId;
-    }
-    const template_data = {
-      mirror: SiteHostname + url,
-    };
-    for (const select_selector of select_selectors) {
-      const opt_attrs = document
-        .querySelector(select_selector)
-        .querySelector("option:checked").attributes;
-      for (const attr of opt_attrs) {
-        if (attr.name.startsWith("data-")) {
-          template_data[attr.name.slice(5)] = attr.value;
-        }
-      }
-    }
-    // special hack for case-insensitive
-    if ("sudoe" in template_data) {
-      template_data["sudoE"] = template_data.sudoe;
-    }
-    const template = document
-      .querySelector(template_selector)
-      .textContent.trim();
-    const content = Mark.up(template, template_data);
-    target.innerHTML = content;
-    hljs.highlightElement(target);
-  }
-};
-
-Array.from(document.querySelectorAll("select.content-select")).map((el) => {
-  el.addEventListener("change", update_target);
-  el.dispatchEvent(new Event("change"));
-});
+const mirrorId = document.getElementById("zhelp-tmpls")["zhelp-name"] as string;
 
 document.getElementById("help-select").addEventListener("change", (ev) => {
   let help_url = (ev.target as Element).querySelector("option:checked")
