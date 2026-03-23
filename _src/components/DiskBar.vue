tuna/mirror-web

commit 8abf91aa012772c0b95d96bf787fad7702cef99a

diskbar implement in vue

_src/components/DiskBar.vue
@@ -1,0 +1,59 @@
+<script setup>
+import { DISKINFO_JSON_PATH } from "../lib/consts";
+import { ref, onMounted, nextTick } from "vue";
+
+const diskUsages = ref([]);
+
+const readableFileSize = (size) => {
+  var units = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  var i = 0;
+  while (size >= 1024) {
+    size /= 1024;
+    ++i;
+  }
+  return size.toFixed(1) + " " + units[i];
+};
+
+onMounted(async () => {
+  const res = await fetch(DISKINFO_JSON_PATH);
+  const d = await res.json();
+  if (!Array.isArray(d)) {
+    d = [d];
+  }
+  diskUsages.value = d.map((disk) => ({
+    desc: disk.desc,
+    used: readableFileSize(disk.used_kb * 1024),
+    total: readableFileSize(disk.total_kb * 1024),
+    percentage: 0,
+    _percentage: Math.round((disk.used_kb * 100) / disk.total_kb),
+  }));
+  await nextTick();
+  await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve();
+    }, 0),
+  );
+  diskUsages.value.forEach((disk) => {
+    disk.percentage = disk._percentage;
+  });
+});
+</script>
+<template>
+  <div class="col-12" v-if="diskUsages.length"><strong>磁盘占用</strong>:</div>
+  <template v-for="disk in diskUsages">
+    <div class="col-1" v-if="disk.desc">{{ disk.desc }}</div>
+    <div :class="['col-' + (disk.desc ? '11' : '12')]">
+      <div
+        class="progress mb-3"
+        role="progressbar"
+        :aria-valuenow="disk.percentage"
+        aria-valuemin="0"
+        aria-valuemax="100"
+      >
+        <div class="progress-bar" :style="{ width: disk.percentage + '%' }">
+          <strong>{{ disk.used }} / {{ disk.total }}</strong>
+        </div>
+      </div>
+    </div>
+  </template>
+</template>

_src/entrypoints/status.js
@@ -1,2 +1,7 @@
 import "./default";
 import "../styles/status.scss";
+
+import DiskBar from "../components/DiskBar.vue";
+import { createApp } from "vue";
+
+createApp(DiskBar).mount("#disk-usage");

_src/lib/consts.js
@@ -1,2 +1,3 @@
 export const TUNASYNC_JSON_PATH = "/static/tunasync.json";
 export const ISOINFO_JSON_PATH = "/static/status/isoinfo.json";
+export const DISKINFO_JSON_PATH = "/static/status/disk.json";

static/js/status.es6
@@ -1,41 +1,6 @@
 ---
 ---
 document.addEventListener("DOMContentLoaded", () => {
-  function readableFileSize(size) {
-    var units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-    var i = 0;
-    while(size >= 1024) {
-      size /= 1024;
-      ++i;
-    }
-    return size.toFixed(1) + ' ' + units[i];
-  }
-
-  const diskUsage = new Vue({
-    el: "#disk-usage",
-    data: {
-      diskUsages: []
-    },
-    created () {
-      fetch("/static/status/disk.json").then((resp)=>resp.json()).then((d) => {
-        if(!Array.isArray(d)){
-          d = [d];
-        }
-        this.diskUsages = d.map((disk) => ({
-          desc: disk.desc,
-          used: readableFileSize(disk.used_kb * 1024),
-          total: readableFileSize(disk.total_kb * 1024),
-          percentage: 0,
-          _percentage: Math.round(disk.used_kb * 100 / disk.total_kb)
-        }));
-        setTimeout(()=>{
-          this.diskUsages.forEach((disk) => {
-            disk.percentage = disk._percentage;
-          });
-        }, 0);
-      });
-    }
-  });
 
   const SCROLL_INTERVAL = 2000;
 

status.html
@@ -20,18 +20,8 @@ jsEntry: status
 				{% include status-pic.html name="service_traffic_IPv6" %}
 			</div>
 			<h4># 磁盘状态 </h4>
-			<div class="row" id="disk-usage">{% raw %}
-				<div class="col-12" v-if="diskUsages.length"><strong>磁盘占用</strong>: </div>
-				<template v-for="disk in diskUsages">
-				<div class="col-1" v-if="disk.desc">{{ disk.desc }}</div>
-				<div :class="['col-' + (disk.desc ? '11' : '12')]">
-				<div class="progress mb-3" role="progressbar" :aria-valuenow="disk.percentage" aria-valuemin="0" aria-valuemax="100">
-					<div class="progress-bar" :style="{width: disk.percentage + '%'}">
-						<strong>{{ disk.used }} / {{ disk.total }}</strong>
-					</div>
-				</div>
-				</div>
-				</template>{% endraw %}
+			<div class="row">
+				<div id="disk-usage"></div>
 				{% include status-pic.html name="diskstats_iops" %}
 				{% include status-pic.html name="diskstats_throughput" %}
 			</div>

commit 630c549767c45a28cc329501514ef11e386fc21a

DiskBar: fix reassign to const variable
Fixes: 8abf91aa0127 ("diskbar implement in vue")

_src/components/DiskBar.vue
@@ -16,10 +16,13 @@ const readableFileSize = (size) => {
 
 onMounted(async () => {
   const res = await fetch(DISKINFO_JSON_PATH);
-  const d = await res.json();
-  if (!Array.isArray(d)) {
-    d = [d];
-  }
+  const d = await (async () => {
+    let d = await res.json();
+    if (!Array.isArray(d)) {
+      d = [d];
+    }
+    return d;
+  })();
   diskUsages.value = d.map((disk) => {
     const percentage = Math.round((disk.used_kb * 100) / disk.total_kb);
     return {