3da6b7d29b64589d678aa8adf8cca8aa23c8956e
Liu Xiaoyi <circuitcoder0@gmail.com>
2026-04-01 21:30:02 +0800

    Temporarily disables global search box focusing

--- _src/components/SearchBox.vue
+++ _src/components/SearchBox.vue
@@ -2,28 +2,11 @@
 import { defineModel, ref, onMounted, onBeforeUnmount } from "vue";
 const model = defineModel({ type: String, default: "" });
 
-const inputRef = ref(null);
-
-const onGlobalKeyPress = (e) => {
-  if (e.key === "/" && document.activeElement !== inputRef.value) {
-    e.preventDefault();
-    inputRef.value.focus();
-  }
-};
-
-onMounted(() => {
-  window.addEventListener("keypress", onGlobalKeyPress);
-});
-
-onBeforeUnmount(() => {
-  window.removeEventListener("keypress", onGlobalKeyPress);
-});
 </script>
 <template>
   <input
     type="search"
     v-model="model"
-    ref="inputRef"
     placeholder="按 / 搜索"
     autocomplete="off"
     class="ms-auto d-inline-flex align-self-center d-bs3-none"
